cc4cd163304c7e9a3a4a187f7750171c0e5a0aa7
Javier Quiroz <javier.s.quiroz@gmail.com>
2016-01-15 16:34:26 -0800

    foundation laid with webpack cordova and react

---
+++ platforms/browser/www/js/components/WizardDuel.react.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var WizardDuel = React.createClass({
+  render: function(){
+    return (
+      <h2>Welcome to Wizard Duel!</h2>
+    )
+  }
+})
+module.exports = WizardDuel;
---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var WizardDuel = require('./components/WizardDuel.react');
+
+ReactDOM.render(
+  <WizardDuel />,
+  document.getElementById('main')
+);
